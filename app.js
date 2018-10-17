9e4647a53104f0b79ae2b923520cc5535f8c9a7d
Sing Painter <mail@erlan.xyz>
2018-10-18 00:33:38 +0300

    #80

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿initHtmlElements([ '#loading', '#start', '#video', '#map', '#fairytale-page', '#sound', '#btn-map', '#fullscreen-in-btn', '#fullscreen-out-btn', '#music-toggle-btn', '#team', '#authors-btn', '#btn-map-menu', '#logo', '#fairytale-text', '#about', '#about-btn', '#langs' ]);
+﻿initHtmlElements([ '#loading', '#start', '#video', '#map', '#fairytale-page', '#sound', '#btn-map', '#fullscreen-in-btn', '#fullscreen-out-btn', '#music-off-btn', '#music-on-btn', '#team', '#authors-btn', '#btn-map-menu', '#logo', '#fairytale-text', '#about', '#about-btn', '#langs', '#map360-btn', '#video-layer-block' ]);
 
 var soundWidget = SC.Widget('sound');
 
@@ -129,34 +129,73 @@ var initMap = function() {
 			streetViewControl: false,
 			zoomControl: false
 		});
-		map.data.loadGeoJson('fairytales.json?201810171845');
-		map.data.setStyle(function(feature) {
-			return {
-				icon: {
-					url: 'img/myth_icons/' + feature.getProperty('name') + '.png',
-					scaledSize: new google.maps.Size(64, 64)
-				}
-			};
-		});
+		map.data.loadGeoJson('geo.json');
+		mapSetStyle();
 		map.data.addListener('click', function(event) {
-			const name = event.feature.getProperty('name');
-			//const position = event.feature.getGeometry().get();
-			mythAudio = event.feature.getProperty('audio')[currentLang];
-			mythVideo = event.feature.getProperty('video');
-			window.location.hash = 'fairytale/' + name;
+			switch (event.feature.getProperty('type')) {
+				case 'myth': {
+					const name = event.feature.getProperty('name');
+					//const position = event.feature.getGeometry().get();
+					mythAudio = event.feature.getProperty('audio')[currentLang];
+					mythVideo = event.feature.getProperty('video');
+					window.location.hash = 'fairytale/' + name;
+				} break;
+				case 'video': {
+					mythVideo = event.feature.getProperty('video');
+					window.location.hash = 'video360/' + mythVideo;
+				} break;
+				case 'photo': {
+					//
+				} break;
+			}
 		});
 	//});
 }
 
-$musicToggleBtn.addEventListener('click', function() {
+var mapTypeShow = 'myths';
+
+var mapSetStyle = function() {
+	if (map) {
+		map.data.setStyle(function(feature) {
+			var markerStyle = {
+				icon: {
+					scaledSize: new google.maps.Size(64, 64)
+				}
+			};
+			switch (feature.getProperty('type')) {
+				case 'myth': {
+					if (mapTypeShow != 'myths') markerStyle.visible = false;
+					markerStyle.icon.url = 'img/myth_icons/' + feature.getProperty('name') + '.png';
+				} break;
+				case 'video': {
+					if (mapTypeShow != '360') markerStyle.visible = false;
+					markerStyle.icon.url = 'img/video_360.png';
+				} break;
+				case 'photo': {
+					if (mapTypeShow != '360') markerStyle.visible = false;
+					markerStyle.icon.url = 'img/photo_360.png';
+				} break;
+			}
+			return markerStyle;
+		});
+	}
+}
+
+$musicOffBtn.addEventListener('click', function() {
 	soundWidget.isPaused(function(isPaused) {
 		if ( ! isPaused) {
 			soundWidget.pause();
-			$musicToggleBtn.innerText = 'Вкл. музыку';
+			$musicOffBtn.style.display = 'none';
+			$musicOnBtn.style.display = 'inline-block';
 		}
-		else {
+	});
+});
+$musicOnBtn.addEventListener('click', function() {
+	soundWidget.isPaused(function(isPaused) {
+		if (isPaused) {
 			soundWidget.play();
-			$musicToggleBtn.innerText = 'Выкл. музыку';
+			$musicOnBtn.style.display = 'none';
+			$musicOffBtn.style.display = 'inline-block';
 		}
 	});
 });
@@ -209,6 +248,7 @@ window.addEventListener('hashchange', function() {
 			switch (params[0]) {
 				case 'fairytale': {
 					console.log('fairytale', params[1]);
+					$videoLayerBlock.style.display = 'block';
 					$map.style.display = 'none';
 					$start.style.display = 'none';
 					$logo.style.display = 'none';
@@ -240,16 +280,51 @@ window.addEventListener('hashchange', function() {
 							fairytaleText.reset();
 						});
 					});
-					$musicToggleBtn.style.display = 'none';
+					$musicOffBtn.style.display = 'none';
+					$musicOnBtn.style.display = 'none';
 					$btnMapMenu.style.display = 'none';
 					$btnMap.style.display = 'inline-block';
+					initFullscreenInBtn();
+				}; break;
+				case 'video360': {
+					console.log('video360', params[1]);
+					$videoLayerBlock.style.display = 'none';
+					$map.style.display = 'none';
+					$start.style.display = 'none';
+					$logo.style.display = 'none';
+					$authorsBtn.style.display = 'none';
+					$aboutBtn.style.display = 'none';
+					$team.style.display = 'none';
+					$about.style.display = 'none';
+					$fairytalePage.style.display = 'none';
+					if (player) player.loadVideoById(mythVideo);
+					soundWidget.bind(SC.Widget.Events.READY, function() {
+						soundWidget.pause();
+					});
+					$musicOffBtn.style.display = 'none';
+					$musicOnBtn.style.display = 'none';
+					$btnMapMenu.style.display = 'none';
+					$map360Btn.style.display = 'inline-block';
 					initFullscreenInBtn();
 				}; break;
 			}
 		}
 		else {
 			switch (params[0]) {
-				case 'map': {
+				case 'map': case '360': {
+					//map.setCenter({ lat: 52.7, lng: 23.9 });
+					if (params[0] == 'map') {
+						mapTypeShow = 'myths';
+						mapSetStyle();
+						$map360Btn.style.display = 'inline-block';
+						$btnMapMenu.style.display = 'none';
+					}
+					else {
+						mapTypeShow = '360';
+						mapSetStyle();
+						$btnMapMenu.style.display = 'inline-block';
+						$map360Btn.style.display = 'none';
+					}
 					$authorsBtn.style.display = 'inline-block';
 					$aboutBtn.style.display = 'inline-block';
 					$map.style.display = 'block';
@@ -258,9 +333,8 @@ window.addEventListener('hashchange', function() {
 					$logo.style.left = '20px';
 					$start.style.display = 'none';
 					$team.style.display = 'none';
-					$about.style.display = 'block';
+					$about.style.display = 'none';
 					$btnMap.style.display = 'none';
-					$btnMapMenu.style.display = 'none';
 					if (player) player.stopVideo();
 					initFullscreenInBtn();
 					soundWidget.bind(SC.Widget.Events.READY, function() {
@@ -268,13 +342,13 @@ window.addEventListener('hashchange', function() {
 							if (sound.id != 499380882) playSound(499380882, true);
 						});
 					});
-					$musicToggleBtn.style.display = 'inline-block';
-					$musicToggleBtn.innerText = 'Выкл. музыку';
+					$musicOffBtn.style.display = 'inline-block';
 				}; break;
 				case 'team': {
 					$team.style.display = 'block';
 					$btnMapMenu.style.display = 'inline-block';
 					$aboutBtn.style.display = 'inline-block';
+					$map360Btn.style.display = 'inline-block';
 					$about.style.display = 'none';
 					$authorsBtn.style.display = 'none';
 					$logo.style.display = 'none';
@@ -289,8 +363,7 @@ window.addEventListener('hashchange', function() {
 							if (sound.id != 504406362) playSound(504406362, true);
 						});
 					});
-					$musicToggleBtn.style.display = 'inline-block';
-					$musicToggleBtn.innerText = 'Выкл. музыку';
+					$musicOffBtn.style.display = 'inline-block';
 					$team.style.top = window.innerHeight + 'px';
 					setTimeout(teamAnimation, 3000);
 				}; break;
@@ -298,6 +371,7 @@ window.addEventListener('hashchange', function() {
 					$about.style.display = 'block';
 					$btnMapMenu.style.display = 'inline-block';
 					$authorsBtn.style.display = 'inline-block';
+					$map360Btn.style.display = 'inline-block';
 					$team.style.display = 'none';
 					$aboutBtn.style.display = 'none';
 					$logo.style.display = 'none';
@@ -312,8 +386,7 @@ window.addEventListener('hashchange', function() {
 							if (sound.id != 513805107) playSound(513805107, true);
 						});
 					});
-					$musicToggleBtn.style.display = 'inline-block';
-					$musicToggleBtn.innerText = 'Выкл. музыку';
+					$musicOffBtn.style.display = 'inline-block';
 				}; break;
 			}
 		}
